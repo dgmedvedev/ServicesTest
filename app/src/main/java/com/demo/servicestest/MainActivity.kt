60fe7142db3c7e8bb4921cb91e3fdebb3bd94ccd
dgmedvedev <dg-medvedev@mail.ru>
2023-07-27 13:40:38 +0300

    Initial commit. Add dependencies

---
+++ app/src/main/java/com/demo/servicestest/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.demo.servicestest
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.demo.servicestest.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private val binding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+    }
+}
