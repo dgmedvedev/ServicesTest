e46da892fd0b57f827abc8b87a83e98aabfb46a0
dgmedvedev <dg-medvedev@mail.ru>
2023-07-28 16:17:37 +0300

    notification complete

--- app/src/main/java/com/demo/servicestest/MainActivity.kt
+++ app/src/main/java/com/demo/servicestest/MainActivity.kt
@@ -1,7 +1,11 @@
 package com.demo.servicestest
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.app.NotificationCompat
 import com.demo.servicestest.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -16,5 +20,35 @@ class MainActivity : AppCompatActivity() {
         binding.simpleService.setOnClickListener {
             startService(MyService.newIntent(this, 25))
         }
+
+        binding.foregroundService.setOnClickListener {
+            showNotification()
+        }
+    }
+
+    private fun showNotification() {
+        val notificationManager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val notificationChannel = NotificationChannel(
+                CHANNEL_ID, // id канала
+                CHANNEL_NAME, // id канала
+                NotificationManager.IMPORTANCE_DEFAULT // значение в зависимости от приоритета
+            )
+            notificationManager.createNotificationChannel(notificationChannel)
+        }
+        val notification = NotificationCompat.Builder(this, CHANNEL_ID)
+            .setContentTitle("Title")
+            .setContentText("Text")
+            .setSmallIcon(R.drawable.ic_launcher_background)
+            .build()
+        notificationManager.notify(
+            1,
+            notification
+        ) // <uses-permission android:name="android.permission.POST_NOTIFICATIONS" />
+    }
+
+    companion object {
+        private const val CHANNEL_ID = "channel_id"
+        private const val CHANNEL_NAME = "channel_name"
     }
 }
