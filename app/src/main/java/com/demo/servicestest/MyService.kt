f63fcba58c40aaa24da02ce3058951b49dd8a63b
dgmedvedev <dg-medvedev@mail.ru>
2023-07-28 13:32:36 +0300

    add intent, add coroutines

--- app/src/main/java/com/demo/servicestest/MyService.kt
+++ app/src/main/java/com/demo/servicestest/MyService.kt
@@ -5,8 +5,12 @@ import android.content.Context
 import android.content.Intent
 import android.os.IBinder
 import android.util.Log
+import kotlinx.coroutines.*
 
 class MyService : Service() {
+
+    private val coroutineScope = CoroutineScope(Dispatchers.Main)
+
     override fun onCreate() {
         super.onCreate()
         log("onCreate()")
@@ -14,15 +18,19 @@ class MyService : Service() {
 
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
         log("onStartCommand()")
-        for (i in 0 until 100) {
-            Thread.sleep(1000)
-            log("Timer $i")
+        val start = intent?.getIntExtra(EXTRA_START, 0) ?: 0
+        coroutineScope.launch {
+            for (i in start until start + 100) {
+                delay(1000)
+                log("Timer $i")
+            }
         }
-        return super.onStartCommand(intent, flags, startId)
+        return START_REDELIVER_INTENT
     }
 
     override fun onDestroy() {
         super.onDestroy()
+        coroutineScope.cancel()
         log("onDestroy()")
     }
 
@@ -35,8 +43,12 @@ class MyService : Service() {
     }
 
     companion object {
-        fun newIntent(context: Context): Intent {
-            return Intent(context, MyService::class.java)
+        private const val EXTRA_START = "start"
+
+        fun newIntent(context: Context, start: Int): Intent {
+            return Intent(context, MyService::class.java).apply {
+                putExtra(EXTRA_START, start)
+            }
         }
     }
 }
