c1cd46e9a866feb26fcccb8446729fb417b5668f
dgmedvedev <dg-medvedev@mail.ru>
2023-08-03 18:40:34 +0300

    JobIntentService complete

--- app/src/main/java/com/demo/servicestest/MainActivity.kt
+++ app/src/main/java/com/demo/servicestest/MainActivity.kt
@@ -58,5 +58,8 @@ class MainActivity : AppCompatActivity() {
                 startService(MyIntentService2.newIntent(this, page++))
             }
         }
+        binding.jobIntentService.setOnClickListener {
+            MyJobIntentService.enqueue(this, page++)
+        }
     }
 }
