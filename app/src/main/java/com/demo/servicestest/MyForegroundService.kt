89be1cb517ead53f6d08ea433a28df88142363ab
dgmedvedev <dg-medvedev@mail.ru>
2023-09-28 14:52:04 +0300

    progress_in_notification complete

--- app/src/main/java/com/demo/servicestest/MyForegroundService.kt
+++ app/src/main/java/com/demo/servicestest/MyForegroundService.kt
@@ -1,6 +1,5 @@
 package com.demo.servicestest
 
-import android.app.Notification
 import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.app.Service
@@ -16,16 +15,28 @@ class MyForegroundService : Service() {
 
     private val coroutineScope = CoroutineScope(Dispatchers.Main)
 
+    private val notificationManager by lazy {
+        getSystemService(NOTIFICATION_SERVICE) as NotificationManager
+    }
+
+    private val notificationBuilder by lazy {
+        createNotificationBuilder()
+    }
+
     override fun onCreate() {
         super.onCreate()
         createNotificationChannel()
-        startForeground(NOTIFICATION_ID, createNotification())
+        startForeground(NOTIFICATION_ID, notificationBuilder.build())
     }
 
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
         coroutineScope.launch {
-            for (i in 0 until 100) {
+            for (i in 0..100 step 5) {
                 delay(1000)
+                val notification = notificationBuilder
+                    .setProgress(100, i, false)
+                    .build()
+                notificationManager.notify(NOTIFICATION_ID, notification)
                 log("Timer $i")
             }
             stopSelf()
@@ -48,7 +59,6 @@ class MyForegroundService : Service() {
     }
 
     private fun createNotificationChannel() {
-        val notificationManager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             val notificationChannel = NotificationChannel(
                 CHANNEL_ID,
@@ -59,13 +69,12 @@ class MyForegroundService : Service() {
         }
     }
 
-    private fun createNotification(): Notification {
-        return NotificationCompat.Builder(this, CHANNEL_ID)
-            .setContentTitle("Title_foreground")
-            .setContentText("Text_foreground")
-            .setSmallIcon(R.drawable.ic_launcher_background)
-            .build() // <uses-permission android:name="android.permission.POST_NOTIFICATIONS" />
-    }
+    private fun createNotificationBuilder() = NotificationCompat.Builder(this, CHANNEL_ID)
+        .setContentTitle("Title_foreground")
+        .setContentText("Text_foreground")
+        .setSmallIcon(R.drawable.ic_launcher_background)
+        .setProgress(100, 0, false)
+        .setOnlyAlertOnce(true)
 
     companion object {
         private const val NOTIFICATION_ID = 1
